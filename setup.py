7dc9d50e85dc85289e3c23a08147a4da93530d52
quynhdo <quynhdtn.hut@gmail.com>
2016-10-24 15:21:40 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CoreNLP',
+    version='1.0',
+    packages=['liir', 'liir.nlp', 'liir.nlp.io', 'liir.nlp.features', 'liir.nlp.representation'],
+    url='',
+    license='',
+    author='quynhdo',
+    author_email='quynhdtn.hut@gmail.com',
+    description='Core NLP classes including feature, representation and Reader classes'
+)
